8076feaa126d2e4c60ee1f6a84a1bcf5c7323f48
RFOSHA <ryan.m.foshage@gmail.com>
2024-08-03 14:32:30 -0500

    added quick sim logic

--- routers/home.py
+++ routers/home.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from functs.run_multiple_battle_sim import run_multiple_battle_sims
 
 import json
 
@@ -45,3 +46,73 @@ async def delete_battle(request: Request, name: str):
         return JSONResponse(status_code=200, content={"message": "Battle deleted successfully"})
     else:
         return JSONResponse(status_code=404, content={"message": "Battle not found"})
+
+@router.post("/quick_simulate", response_class=HTMLResponse)
+async def quick_simulate(request: Request,
+                         attack_infantry: int = Form(0),
+                         attack_artillery: int = Form(0),
+                         attack_tank: int = Form(0),
+                         attack_fighter: int = Form(0),
+                         attack_bomber: int = Form(0),
+                         attack_submarine: int = Form(0),
+                         attack_destroyer: int = Form(0),
+                         attack_cruiser: int = Form(0),
+                         attack_aircraft_carrier: int = Form(0),
+                         attack_battleship: int = Form(0),
+                         attack_aa: int = Form(0),
+                         defense_infantry: int = Form(0),
+                         defense_artillery: int = Form(0),
+                         defense_tank: int = Form(0),
+                         defense_fighter: int = Form(0),
+                         defense_bomber: int = Form(0),
+                         defense_submarine: int = Form(0),
+                         defense_destroyer: int = Form(0),
+                         defense_cruiser: int = Form(0),
+                         defense_aircraft_carrier: int = Form(0),
+                         defense_battleship: int = Form(0),
+                         defense_aa: int = Form(0),
+                         number_of_simulations: int = Form(1000)):
+
+    attacking_units = {
+        "Infantry": attack_infantry,
+        "Artillery": attack_artillery,
+        "Tank": attack_tank,
+        "Fighter": attack_fighter,
+        "Bomber": attack_bomber,
+        "Submarine": attack_submarine,
+        "Destroyer": attack_destroyer,
+        "Cruiser": attack_cruiser,
+        "Aircraft Carrier": attack_aircraft_carrier,
+        "Battleship": attack_battleship,
+        "AA": attack_aa
+    }
+
+    defending_units = {
+        "Infantry": defense_infantry,
+        "Artillery": defense_artillery,
+        "Tank": defense_tank,
+        "Fighter": defense_fighter,
+        "Bomber": defense_bomber,
+        "Submarine": defense_submarine,
+        "Destroyer": defense_destroyer,
+        "Cruiser": defense_cruiser,
+        "Aircraft Carrier": defense_aircraft_carrier,
+        "Battleship": defense_battleship,
+        "AA": defense_aa
+    }
+
+    form_data = dict(request.query_params)
+    saved_battles = request.session.get("saved_battles", {})
+    # initial_attacking_units = {unit: count for unit, count in attacking_units.items() if count > 0}
+    # initial_defending_units = {unit: count for unit, count in defending_units.items() if count > 0}
+
+    attacker_win_count, defender_win_count, ties, attacker_remaining_units_count, defender_remaining_units_count, battle_history_attacking_df, battle_history_defending_df = run_multiple_battle_sims(
+        attacking_units, defending_units, number_of_simulations)
+
+    print(attacker_win_count, defender_win_count, ties)
+
+    return JSONResponse({
+        "attacker_win_count": attacker_win_count,
+        "defender_win_count": defender_win_count,
+        "ties": ties,
+    })
